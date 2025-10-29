2558807ae18113aa7f5e90dc7bbf434b3cbf58a5
Binukumari V <binukumariv2002@gmail.com>
2025-10-29 14:10:03 +0530

    practice

---
+++ d61.sql
@@ -0,0 +1,21 @@
+INSERT INTO booktb(id,title,author,genre,price,copies_sold)
+      VALUES(1, 'The Silent Patient', 'Alex Michaelides', 'Thriller', 399.00, 1200),
+            (2, 'Atomic Habits', 'James Clear', 'Self-help', 499.00, 2000),
+            (3, 'The Psychology of Money', 'Morgan Housel', 'Finance', 350.00, 1800);
+INSERT INTO bestsellers(id,title,author,genre,price,copies_sold)
+   VALUES(4, 'Ikigai', 'Francesc Miralles', 'Philosophy', 300.00, 2500),
+         (5, 'Think Like a Monk', 'Jay Shetty', 'Self-help', 450.00, 2200);
+SELECT title,author
+FROM bestsellers
+UNION
+SELECT title,author
+FROM booktb;
+SELECT title
+SELECT title
+FROM booktb
+WHERE price > 400;
+SELECT AVG(price)
+FROM booktb
+SELECT COUNT(id)
+FROM booktb;
+SELECT title AS 'Book Title',author AS 'Written By' FROM booktb 
